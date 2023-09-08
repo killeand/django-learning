9fd148da91376e8dbe64d35b06815b7eb1e3330f
Jason Johnston <killean@shaw.ca>
2023-09-08 11:26:06 -0500

    Working on understanding django routing

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,5 +2,6 @@ from django.urls import re_path, include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', include('api.urls'))
+    path('api/', include('api.urls')),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'))
 ]
--- frontend/src/pages/Calls.jsx
+++ frontend/src/pages/Calls.jsx
@@ -5,15 +5,21 @@ export default function Page() {
     const [alpha, setAlpha] = useState("");
 
     function RunAlpha() {
-        axios
+        axios.get("/api")
+            .then(({ data }) => {
+                setAlpha(data);
+            })
+            .catch(({response:error}) => {
+                setAlpha(error.data);
+            });
     }
 
     return (
         <>
             <h1>API Calls</h1>
             <div className="border border-red-800 p-2 flex flex-col items-stretch">
-                <div className="border-b-2 border-black pb-2 mb-2">{alpha}</div>
-                <button className="btn btn-primary" onClick={()=>RunAlpha()}>Run Alpha</button>
+                <div className="border-b-2 border-black pb-2 mb-2"><pre>{JSON.stringify(alpha, null, '\t')}</pre></div>
+                <button className="btn btn-primary" onClick={()=>RunAlpha()}>/api</button>
             </div>
         </>
     );
