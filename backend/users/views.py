7c9f2e52ce45290f0fbdfe5b3c550b5ea15e992e
Jason Johnston <killean@shaw.ca>
2023-09-28 15:45:00 -0500

    Trying to work on user permissions and data

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,8 +1,21 @@
-from rest_framework import viewsets, permissions
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import BasePermission
 from django.contrib.auth import get_user_model
 from .serializers import UserSerializer
 
-class UserView(viewsets.ModelViewSet):
+class UserPermission(BasePermission):
+    def has_permission(self, request, view):
+        return True
+    
+    def has_object_permission(self, request, view, obj):
+        return True
+
+class UserView(ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [UserPermission]
+    
+class LimitedUserView(ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [UserPermission]
