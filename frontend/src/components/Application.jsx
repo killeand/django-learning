6f5efa835bc718c1a6a2504d85a2a87b097e31fd
Jason Johnston <killean@shaw.ca>
2023-10-12 21:22:25 -0500

    Doing a bit more conversion. Given test site a funny concept.

--- frontend/src/components/Application.jsx
+++ frontend/src/components/Application.jsx
@@ -2,29 +2,39 @@ import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
 import { useContext } from "react";
 import UserContext from "./UserContext";
 import Calls from '@/pages/Calls';
-import Login from '@/pages/Login';
-import Logout from '@/pages/Logout';
+import SiteIndex from '@/pages/Index';
+import Login from '@/pages/auth/Login';
+import Logout from '@/pages/auth/Logout';
 
-import HomeIcon from '@mui/icons-material/Home';
-import { Sheet, Box, List, ListItem, ListItemButton, ListItemContent, Typography as T } from "@mui/joy";
+import { Home, Password, Lock, Phone, Settings, Shield, Error, DarkModeRounded, LightMode } from "@mui/icons-material";
+import { Sheet, Box, List, ListItem, ListItemButton, ListItemContent, Typography as T, IconButton } from "@mui/joy";
+import { useColorScheme } from "@mui/joy";
 
 export default function Application() {
     const context = useContext(UserContext);
+    const { mode, setMode } = useColorScheme();
+    const ActiveIcon = (mode === "light") ? DarkModeRounded : LightMode;
 
     const URLS = [
-        { path: "/", index: true, name: "Home", authed: false, always: true, component: (<p>Index Page</p>) },
-        { path: "/login", index: false, name: "Login", authed: false, always: false, component: (<Login />) },
-        { path: "/logout", index: false, name: "Logout", authed: true, always: false, component: (<Logout />) },
-        { path: "/calls", index: false, name: "API Calls", authed: true, always: false, component: (<Calls />) },
-        { path: "/api", index: false, name: "Django: API", authed: false, always: true, component: null },
-        { path: "/admin", index: false, name: "Django: Admin", authed: false, always: true, component: null },
-        { path: "/errored", index: false, name: "Error", authed: false, always: true, component: null },
+        { path: "/", index: true, name: "Home", authed: false, always: true, component: (<SiteIndex />), icon: Home },
+        { path: "/login", index: false, name: "Login", authed: false, always: false, component: (<Login />), icon: Password },
+        { path: "/logout", index: false, name: "Logout", authed: true, always: false, component: (<Logout />), icon: Lock },
+        { path: "/calls", index: false, name: "API Calls", authed: true, always: false, component: (<Calls />), icon: Phone },
+        { path: "/api", index: false, name: "Django: API", authed: false, always: true, component: null, icon: Settings },
+        { path: "/admin", index: false, name: "Django: Admin", authed: false, always: true, component: null, icon: Shield },
+        { path: "/errored", index: false, name: "Error", authed: false, always: true, component: null, icon: Error },
     ];
 
     return (
         <BrowserRouter>
-            <Sheet component="header" sx={{height:'100dvh', width:'16%', top:0, p:2, flexShrink:0, display:'flex', flexDirection:'column', gap:2, borderRight:'1px solid',borderColor:'divider'}}>
-                <Box component="nav" sx={{minHeight:0,overflow:'hidden auto',flexGrow:1,display:'flex',flexDirection:'column'}}>
+            <Sheet component="header" sx={{width:'16%', top:0, p:2, flexShrink:0, display:'flex', flexDirection:'column', gap:2, borderRight:'1px solid',borderColor:'divider'}}>
+                <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+                    <T level="title-lg" sx={{ flexGrow: 1 }}>STSM</T>
+                    <IconButton size="sm" color={(mode==="light")?"primary":"neutral"}  onClick={()=>setMode((mode==="light")?"dark":"light")}>
+                        <ActiveIcon />
+                    </IconButton>
+                </Box>
+                <Box component="nav" sx={{ minHeight: 0, overflow: 'hidden auto', flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
                     <List size="sm" sx={{gap:1,'--List-nestedInsetStart':'30px','--ListItem-radius':(theme) => theme.vars.radius.sm}}>
                         {URLS.map((item, index) => {
                             let comp = (item.component == null) ? "a" : Link;
@@ -34,7 +44,7 @@ export default function Application() {
                                 return (
                                     <ListItem key={`nav-${index}-${item.name}`} component={comp} {...path} sx={{'&:hover':{textDecoration:'none'}}}>
                                         <ListItemButton>
-                                            <HomeIcon />
+                                            <item.icon />
                                             <ListItemContent>
                                                 <T level="title-sm">{item.name}</T>
                                             </ListItemContent>
@@ -45,7 +55,7 @@ export default function Application() {
                     </List>
                 </Box>
             </Sheet>
-            <main>
+            <Sheet component="main" sx={{flexGrow:1}}>
                 <Routes>
                     {URLS.map((item, index) => {
                         let addindex = {};
@@ -57,7 +67,7 @@ export default function Application() {
                     <Route path="/404" element={<div></div>} />
                     <Route path="*" element={<Navigate to="/404" />} />
                 </Routes>
-            </main>
+            </Sheet>
         </BrowserRouter>
     );
 }
